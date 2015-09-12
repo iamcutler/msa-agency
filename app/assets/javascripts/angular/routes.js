28f891bfa1303b56b1e374cee67f21c2c07db991
Allan Cutler <iamcutler@icloud.com>
2015-09-12 11:07:57 -0700

    Added HTML5 push state to angular app

--- app/assets/javascripts/angular/routes.js
+++ app/assets/javascripts/angular/routes.js
@@ -1,9 +1,17 @@
 // Angular routes
 
 angular.module('MSAAgency')
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+    // Default page if no routes are found
     $urlRouterProvider.otherwise("/");
 
+    // Enable HTML5 push state
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+
+    // Routes
     $stateProvider
         .state('app', {
             url: '/',
