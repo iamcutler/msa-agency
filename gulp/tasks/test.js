897ae907b785ef9a0e6dba4aa6f3e3bf28e7fb7d
Allan Cutler <iamcutler@icloud.com>
2015-09-11 21:02:12 -0700

    Added karma and jasmine for client side testing and configured RSpec under server directory.

---
+++ gulp/tasks/test.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var Server = require('karma').Server;
+
+gulp.task('test', function(done) {
+    new Server({
+        configFile: __dirname + '/../../karma.conf.js',
+        singleRun: true
+    }, done).start();
+});
+
+gulp.task('tdd', function(done) {
+    new Server({
+        configFile: __dirname + '/../../karma.conf.js'
+    }, done).start();
+});
