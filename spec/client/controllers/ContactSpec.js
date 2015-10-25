237d131578f3dfd25d546c5b9747e91e7d3f4114
Allan Cutler <iamcutler@icloud.com>
2015-10-25 11:06:17 -0700

    test(contact): Added specs for default form properties.

--- spec/client/controllers/ContactSpec.js
+++ spec/client/controllers/ContactSpec.js
@@ -21,4 +21,28 @@ describe('Controller: Contact', () => {
 
         expect(ContactCtrl).toBeDefined();
     });
+
+    describe('Contact Form', () => {
+        describe('Default props', () => {
+            it('should have regarding', () => {
+                expect(ContactCtrl.contact_form.hasOwnProperty('regarding')).toBe(true);
+            });
+
+            it('should have name', () => {
+                expect(ContactCtrl.contact_form.hasOwnProperty('name')).toBe(true);
+            });
+
+            it('should have email', () => {
+                expect(ContactCtrl.contact_form.hasOwnProperty('email')).toBe(true);
+            });
+
+            it('should have phone', () => {
+                expect(ContactCtrl.contact_form.hasOwnProperty('phone')).toBe(true);
+            });
+
+            it('should have message', () => {
+                expect(ContactCtrl.contact_form.hasOwnProperty('message')).toBe(true);
+            });
+        });
+    });
 });
