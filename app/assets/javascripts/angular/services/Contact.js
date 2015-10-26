319f00ee3f38edaa2a42f61d1c62048468e72682
Allan Cutler <iamcutler@icloud.com>
2015-10-25 18:48:02 -0700

    feat(contact service): Introduced submitForm method

--- app/assets/javascripts/angular/services/Contact.js
+++ app/assets/javascripts/angular/services/Contact.js
@@ -1,3 +1,12 @@
 export default class ContactService {
-    constructor() {}
+    // @ngInject
+    constructor($http, CommonService) {
+        this.$http = $http;
+        this.commonService = CommonService;
+    }
+
+    submitForm(form_fields) {
+        return this.$http.post('api/v1/contact')
+            .then(this.commonService.handleHttpResponse('Error while submitting contact form'));
+    }
 }
--- spec/client/services/ContactSpec.js
+++ spec/client/services/ContactSpec.js
@@ -1,15 +1,40 @@
 describe('Service: Contact', () => {
-    var ContactService;
+    var $httpBackend, ContactService;
 
     beforeEach(() => {
         module('MSAAgency.services');
 
         inject($injector => {
+            $httpBackend = $injector.get('$httpBackend');
             ContactService = $injector.get('ContactService');
         });
     });
 
     it('to be defined', () => {
         expect(ContactService).toBeDefined();
+    });
+
+    describe('method: submitForm', () => {
+        let contact_fields = {
+            regarding: 'Bug',
+            name: 'Tony Selznick',
+            email: 'tony@msaagency.com',
+            phone: '3101234567',
+            message: 'This is a bug/feedback'
+        };
+
+        it('should call backend to submit form', (done) => {
+            $httpBackend.whenPOST('api/v1/contact').respond({
+                success: true
+            });
+
+            ContactService.submitForm(contact_fields)
+                .then(data => {
+                    expect(data.success).toBe(true);
+                })
+                .finally(done);
+
+            $httpBackend.flush();
+        });
     });
 });
