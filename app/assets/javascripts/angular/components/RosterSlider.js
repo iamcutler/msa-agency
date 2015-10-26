97b6d84620ed05da71d1b008213e4fc2eafcb358
Allan Cutler <iamcutler@icloud.com>
2015-10-25 22:46:06 -0700

    feat(roster slider): Added navigation arrows

--- app/assets/javascripts/angular/components/RosterSlider.js
+++ app/assets/javascripts/angular/components/RosterSlider.js
@@ -11,20 +11,30 @@ export default function RosterSlider() {
                 <span class="title">{{ title }}</span>
 
                 <div class="roster">
-                    <ul>
-                        <li ng-repeat="member in roster">
-                            <a href="" class="thumbnail" title="">
-                                <img ng-src="{{ member.img }}" alt="{{ member.name }}">
-                                <div class="overlay">
-                                    <span class="arrow"></span>
-                                    <span class="overlay-name">{{ member.name }}</span>
-                                    <span class="overlay-position">{{ member.title }}</span>
-                                </div>
-                            </a>
+                    <div class="navigation left">
+                        <div class="arrow"></div>
+                    </div>
 
-                            <span class="name">{{ member.name }}</span>
-                        </li>
-                    </ul>
+                    <div class="thumbs">
+                        <ul>
+                            <li ng-repeat="member in roster">
+                                <a href="" class="thumbnail" title="">
+                                    <img ng-src="{{ member.img }}" alt="{{ member.name }}">
+                                    <div class="overlay">
+                                        <span class="arrow"></span>
+                                        <span class="overlay-name">{{ member.name }}</span>
+                                        <span class="overlay-position">{{ member.title }}</span>
+                                    </div>
+                                </a>
+
+                                <span class="name">{{ member.name }}</span>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="navigation right">
+                        <div class="arrow"></div>
+                    </div>
                 </div>
             </div>
         `
