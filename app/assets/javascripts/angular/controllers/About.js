50f689159171530870a9ac026e88ca69fb16ea7b
Allan Cutler <iamcutler@icloud.com>
2015-10-29 20:48:38 -0700

    refactor(about): Fire initialize function with ngInit

--- app/assets/javascripts/angular/controllers/About.js
+++ app/assets/javascripts/angular/controllers/About.js
@@ -1,29 +1,15 @@
 export default class AboutController {
-    constructor() {
-        this.staff = {
-            losAngeles: [
-                { name: 'Julie McDonald', title: 'Founder/Sr. Agent', img: 'http://www.msaagency.com/images/staff/74858411100511.jpg' },
-                { name: 'Tony Selznick', title: 'Founder/Sr. Agent', img: 'http://dancemogul.com/news/wp-content/uploads/2013/02/tony.jpg' },
-                { name: 'JC Gutierrez', title: 'Los Angeles and New York Director/Sr. Agent', img: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Two_dancers.jpg' },
-                { name: 'Jenn Procter', title: 'Agent', img: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Two_dancers.jpg' },
-                { name: 'Shelli Marghertis', title: 'Agent, Education Department/Choreography', img: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Two_dancers.jpg' },
-                { name: 'Daniel Shirk', title: 'Talent Payment Processor', img: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Two_dancers.jpg' },
-                { name: 'Brandon Louis', title: 'Agent', img: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Two_dancers.jpg' },
-                { name: 'Chelsea Blake', title: 'Agent, Kids and Teens', img: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Two_dancers.jpg' },
-                { name: 'Megan Hunt', title: 'Jr. Agent, Education Department', img: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Two_dancers.jpg' },
-                { name: 'Mara Boyce', title: 'Executive Assistant', img: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Two_dancers.jpg' },
-                { name: 'David', title: 'Agent', img: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Two_dancers.jpg' },
-                { name: 'Receptionist', title: 'Receptionist', img: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Two_dancers.jpg' },
-                { name: 'Chloe', title: 'Agent', img: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Two_dancers.jpg' }
-            ],
-            newYork: [
-                { name: 'Julie McDonald', title: 'Founder/Sr. Agent', img: 'http://www.msaagency.com/images/staff/74858411100511.jpg' },
-                { name: 'Tony Selznick', title: 'Founder/Sr. Agent', img: 'http://dancemogul.com/news/wp-content/uploads/2013/02/tony.jpg' },
-                { name: 'JC Gutierrez', title: 'Los Angeles and New York Director/Sr. Agent', img: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Two_dancers.jpg' },
-                { name: 'Bre O\'toole', title: 'Associate Director', img: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Two_dancers.jpg' },
-                { name: 'Rebecca Harrell', title: 'Assistant', img: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Two_dancers.jpg' },
-                { name: 'Nikki Della Penta', title: 'Agent', img: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Two_dancers.jpg' }
-            ]
-        };
+    // @ngInject
+    constructor(StaffService) {
+        this.staff = {};
+        this.staffService = StaffService;
+    }
+
+    initialize() {
+        this.staffService.all()
+            .then(response => {
+                this.staff = response;
+            })
+            .catch(err => console.log(err));
     }
 }
