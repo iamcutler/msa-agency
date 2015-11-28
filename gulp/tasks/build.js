44c18978e164ba702a3755ea4e8dc7336fca3b2b
Allan Cutler <iamcutler@icloud.com>
2015-11-27 16:43:14 -0800

    fix(gulp): Removed rev for prod for testing

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -3,7 +3,7 @@ var gulpSequence = require('gulp-sequence')
 
 gulp.task('build', function(cb) {
   var tasks = ['clean',['iconFont', 'images'], ['sass', 'browserify']];
-  if(process.env.RAILS_ENV === 'production') tasks.push('rev');
+  //if(process.env.RAILS_ENV === 'production') tasks.push('rev');
   tasks.push(cb);
   gulpSequence.apply(this, tasks);
 });
