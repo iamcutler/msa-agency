6581290dd81dcf7456bcf96956a8e46d7c082349
Allan Cutler <iamcutler@icloud.com>
2015-12-19 11:21:47 -0800

    feat(staff): Changed staff to not divide by location.

--- app/assets/javascripts/angular/controllers/About.js
+++ app/assets/javascripts/angular/controllers/About.js
@@ -1,15 +1,14 @@
 export default class AboutController {
     // @ngInject
     constructor(StaffService) {
-        this.staff = {};
+        this.staff = [];
         this.staffService = StaffService;
     }
 
     initialize() {
         this.staffService.all()
             .then(response => {
-                this.LAStaff = this.staffService.filterByLocation(response, 'Los Angeles');
-                this.NYStaff = this.staffService.filterByLocation(response, 'New York');
+                this.staff = response;
             })
             .catch(err => console.log(err));
     }
