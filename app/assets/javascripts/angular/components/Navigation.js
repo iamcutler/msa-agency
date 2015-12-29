9fb2ae4a2280dd2d6a9aa2ceefd365e011fcf828
Allan Cutler <iamcutler@icloud.com>
2015-12-29 01:12:40 -0800

    feat(navigation): mobile modifications

--- app/assets/javascripts/angular/components/Navigation.js
+++ app/assets/javascripts/angular/components/Navigation.js
@@ -20,35 +20,37 @@ export default function Navigation() {
                         <a ui-sref="splash">McDonald/Selznick Associates</a>
                     </div>
 
-                    <ul class="main-links">
-                        <li>
-                            <a href="">Home</a>
-                            <ul class="home" resize-element resize-width="false" offset-top="112">
-                                <li><a ui-sref="app.home.los-angeles" toggle-navigation>Los Angeles</a></li>
-                                <li><a ui-sref="app.home.new-york" toggle-navigation>New York</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a ui-sref="app.about" toggle-navigation>About Us</a>
-                            <ul class="about-us" resize-element resize-width="false" offset-top="112">
-                                <li><a ui-sref="app.staff" toggle-navigation>Staff</a></li>
-                                <li><a ui-sref="app.affiliates" toggle-navigation>Affiliations</a></li>
-                                <li><a ui-sref="app.talent.legacy" toggle-navigation>Legacy</a></li>
-                                <li><a ui-sref="app.archives" toggle-navigation>Archives</a></li>
-                                <li><a ui-sref="app.faq" toggle-navigation>FAQ</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="">Talent</a>
-                            <ul class="talent" resize-element resize-width="false" offset-top="112">
-                                <li><a ui-sref="app.talent.category({ category: 'dance' })" toggle-navigation>Dance</a></li>
-                                <li><a ui-sref="app.talent.category({ category: 'creative' })" toggle-navigation>Creative</a></li>
-                                <li><a ui-sref="app.talent.category({ category: 'educators' })" toggle-navigation>Educators</a></li>
-                            </ul>
-                        </li>
-                        <li><a ui-sref="app.news" toggle-navigation>News</a></li>
-                        <li><a ui-sref="app.contact" toggle-navigation>Contact Us</a></li>
-                    <ul>
+                    <div class="main-links">
+                        <ul>
+                            <li>
+                                <a href="">Home</a>
+                                <ul class="home" resize-element resize-width="false" offset-top="112">
+                                    <li><a ui-sref="app.home.los-angeles" toggle-navigation>Los Angeles</a></li>
+                                    <li><a ui-sref="app.home.new-york" toggle-navigation>New York</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a ui-sref="app.about" toggle-navigation>About Us</a>
+                                <ul class="about-us" resize-element resize-width="false" offset-top="112">
+                                    <li><a ui-sref="app.staff" toggle-navigation>Staff</a></li>
+                                    <li><a ui-sref="app.affiliates" toggle-navigation>Affiliations</a></li>
+                                    <li><a ui-sref="app.talent.legacy" toggle-navigation>Legacy</a></li>
+                                    <li><a ui-sref="app.archives" toggle-navigation>Archives</a></li>
+                                    <li><a ui-sref="app.faq" toggle-navigation>FAQ</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="">Talent</a>
+                                <ul class="talent" resize-element resize-width="false" offset-top="112">
+                                    <li><a ui-sref="app.talent.category({ category: 'dance' })" toggle-navigation>Dance</a></li>
+                                    <li><a ui-sref="app.talent.category({ category: 'creative' })" toggle-navigation>Creative</a></li>
+                                    <li><a ui-sref="app.talent.category({ category: 'educators' })" toggle-navigation>Educators</a></li>
+                                </ul>
+                            </li>
+                            <li><a ui-sref="app.news" toggle-navigation>News</a></li>
+                            <li><a ui-sref="app.contact" toggle-navigation>Contact Us</a></li>
+                        <ul>
+                    </div>
                 </div>
             </nav>
         `
