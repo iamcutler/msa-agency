623f4d926dc488ac62e4dfac0c46517b255ec1a4
Allan Cutler <iamcutler@icloud.com>
2016-03-01 22:31:40 -0800

    Replaced footer with office locations

--- app/assets/javascripts/angular/components/MainFooter.js
+++ app/assets/javascripts/angular/components/MainFooter.js
@@ -9,45 +9,7 @@ angular.module('MSAAgency.directives')
             <footer class="main">
                 <go-to-top-button></go-to-top-button>
 
-                <div class="app-container">
-                    <div class="addresses">
-                        <div class="address">
-                            <span class="location">Los Angeles</span><br>
-                            323-957-6680 | 953 Cole Ave Hollywood CA 90038
-                        </div>
-                        <div class="address">
-                            <span class="location">New York</span><br>
-                            646-237-6928 | 1115 Broadway, Suite 1056, New York, NY 10010
-                        </div>
-                    </div>
-
-                    <!--<div class="navigation">
-                        <ul>
-                            <li><a ui-sref="splash">Home</a></li>
-                            <li ng-if="AppCtrl.companyReelURL">
-                                <a ng-href="{{ AppCtrl.companyReelURL }}" class="fancybox">Reel</a>
-                            </li>
-                            <li><a ui-sref="app.news">News</a></li>
-                            <li><a ui-sref="app.contact">Contact Us</a></li>
-                        </ul>
-                        <ul>
-                            <li><a ui-sref="app.about">About Us</a></li>
-                            <li class="sub"><a ui-sref="app.about.staff">Our staff</a></li>
-                            <li class="sub"><a ui-sref="app.affiliates">Affliations</a></li>
-                            <li class="sub"><a ui-sref="app.talent.legacy">Legacy</a></li>
-                            <li class="sub"><a ui-sref="app.archives">Archives</a></li>
-                            <li class="sub"><a ui-sref="app.faq">FAQ</a></li>
-                        </ul>
-                        <ul>
-                            <li>Talent</li>
-                            <li class="sub"><a ui-sref="app.client-page({ slug: 'dance' })">Dance</a></li>
-                            <li class="sub"><a ui-sref="app.client-page({ slug: 'commercial' })">Commercials</a></li>
-                            <li class="sub"><a ui-sref="app.client-page({ slug: 'television-film' })">Televison/Film</a></li>
-                            <li class="sub"><a ui-sref="app.client-page({ slug: 'musical-theatre' })">Musical Theatre</a></li>
-                            <li class="sub"><a ui-sref="app.client-page({ slug: 'kids-teen' })">Kids &amp; Teens</a></li>
-                        </ul>
-                    </div>-->
-                </div>
+                <office-locations></office-locations>
 
                 <div class="clearfix"></div>
 
