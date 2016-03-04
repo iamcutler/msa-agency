d8ea360a4de4a372eb7208f0c09b8e892bad6fe3
Allan Cutler <iamcutler@icloud.com>
2016-03-03 20:39:17 -0800

    fix(news): Fixed news article links.

--- app/assets/javascripts/angular/components/NewsArticle.js
+++ app/assets/javascripts/angular/components/NewsArticle.js
@@ -8,7 +8,7 @@ export default function NewsArticle() {
         },
         template: `
             <article ng-class="{ 'full-article': full }">
-                <a ui-sref="app.news-page({ slug: data.slug })">
+                <a ui-sref="app.news-page({ slug: data.slug })" ng-if="!full">
                     <figure>
                         <img src="{{ data.image.thumb }}">
                     </figure>
@@ -21,9 +21,24 @@ export default function NewsArticle() {
 
                     <span class="timestamp">{{ data.created_at | date:'MMMM d, yyyy hh:mm a' : 'UTC' }}</span>
 
-                    <p ng-if="!full">{{ data.body | limitTo: 400 | stripHTML }}</p>
-                    <p ng-if="full" ng-bind-html="data.body"></p>
+                    <p>{{ data.body | limitTo: 400 | stripHTML }}</p>
                 </a>
+
+                <div ng-if="full">
+                    <figure>
+                        <img src="{{ data.image.thumb }}">
+                    </figure>
+
+                    <header>
+                        <span class="type" ng-if="!full">news</span>
+
+                        <h2>{{ data.subject }}</h2>
+                    </header>
+
+                    <span class="timestamp">{{ data.created_at | date:'MMMM d, yyyy hh:mm a' : 'UTC' }}</span>
+
+                    <p ng-bind-html="data.body"></p>
+                </div>
             </article>
         `
     };
