bf51948d3474dd8ea97a8bacf1c6f85248f0d7a3
Allan Cutler <iamcutler@icloud.com>
2016-03-06 13:52:58 -0800

    updated contact email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,18 +4,18 @@ class ContactMailer < ApplicationMailer
     def contact_form(form_params)
         @params = form_params
 
-        mail(to: 'iamcutler@icloud.com', subject: 'MSA Agency Contact Form')
+        mail(to: 'info@msaagency.com', subject: 'MSA Agency Contact Form')
     end
 
     def booking_form(form_params)
         @params = form_params
 
-        mail(to: 'iamcutler@icloud.com', subject: 'I want to book a client')
+        mail(to: 'info@msaagency.com', subject: 'I want to book a client')
     end
 
     def representation_form(form_params)
         @params = form_params
 
-        mail(to: 'iamcutler@icloud.com', subject: 'I would like MSA to represent me!')
+        mail(to: 'info@msaagency.com', subject: 'I would like MSA to represent me!')
     end
 end
