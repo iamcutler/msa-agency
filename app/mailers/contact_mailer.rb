bf7dfad434498edd2c2f0c0e4f66a9bd25eb5955
Allan Cutler <iamcutler@icloud.com>
2016-03-23 21:18:47 -0700

    Changed email for mailers.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,18 +4,18 @@ class ContactMailer < ApplicationMailer
     def contact_form(form_params)
         @params = form_params
 
-        mail(to: 'info@msaagency.com', subject: 'MSA Agency Contact Form')
+        mail(to: 'infoatmsa@gmail.com', subject: 'MSA Agency Contact Form')
     end
 
     def booking_form(form_params)
         @params = form_params
 
-        mail(to: 'info@msaagency.com', subject: 'I want to book a client')
+        mail(to: 'infoatmsa@gmail.com', subject: 'I want to book a client')
     end
 
     def representation_form(form_params)
         @params = form_params
 
-        mail(to: 'info@msaagency.com', subject: 'I would like MSA to represent me!')
+        mail(to: 'infoatmsa@gmail.com', subject: 'I would like MSA to represent me!')
     end
 end
