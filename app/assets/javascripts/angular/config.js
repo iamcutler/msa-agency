95987dc83822af2d16c63edb36cc9302bd1673e4
Allan Cutler <iamcutler@icloud.com>
2016-05-13 20:48:38 -0700

    feat(routes): Disabled strict mode on routing to allow trailing slashes.

--- app/assets/javascripts/angular/config.js
+++ app/assets/javascripts/angular/config.js
@@ -2,12 +2,14 @@
 
 // @ngInject
 angular.module('MSAAgency')
-.config(function($locationProvider) {
+.config(function($locationProvider, $urlMatcherFactoryProvider) {
     // use the HTML5 History API
     $locationProvider.html5Mode({
         enabled: true,
         requireBase: false
     });
+    // Disable strict mode on routes
+    $urlMatcherFactoryProvider.strictMode(false);
 })
 .run(($rootScope, smoothScroll) => {
     // Scroll to top on route change
