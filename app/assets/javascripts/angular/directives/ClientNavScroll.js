d728480ba76433235a0d9ba3781d5e34444fdf93
Allan Cutler <iamcutler@icloud.com>
2016-09-18 16:26:04 -0700

    fix(client nav scroll): Added $inject array for min issue.

--- app/assets/javascripts/angular/directives/ClientNavScroll.js
+++ app/assets/javascripts/angular/directives/ClientNavScroll.js
@@ -2,25 +2,29 @@ export default function ClientNavScroll() {
     return {
         restrict: 'A',
         scope: false,
-        controller: function($window, $element) {
-            const navHeight = 112;
-            const scrollHeight = 485;
-            const el = $element[0];
-            const winEl = angular.element($window);
-            
-            // Bind scrolling event and eval height to fix navigation
-            winEl.bind('scroll', () => {
-                const scrollY = $window.scrollY || winEl[0].document.documentElement.scrollTop; 
-
-                if(scrollY >= scrollHeight) {
-                    el.style.position = 'fixed';
-                    el.style.top = `${navHeight}px`;
-                    return;
-                }
-
-                el.style.position = 'absolute';
-                el.style.top = 'inherit';
-            });
-        }
+        controller: ClientNavScrollController 
     };
 }
+
+function ClientNavScrollController($window, $element) {
+    const navHeight = 112;
+    const scrollHeight = 485;
+    const el = $element[0];
+    const winEl = angular.element($window);
+    
+    // Bind scrolling event and eval height to fix navigation
+    winEl.bind('scroll', () => {
+        const scrollY = $window.scrollY || winEl[0].document.documentElement.scrollTop; 
+
+        if(scrollY >= scrollHeight) {
+            el.style.position = 'fixed';
+            el.style.top = `${navHeight}px`;
+            return;
+        }
+
+        el.style.position = 'absolute';
+        el.style.top = 'inherit';
+    });
+}
+
+ClientNavScrollController.$inject = ['$window', '$element'];
