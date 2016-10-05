e67486eb7364bf662d08e9266f5618c176e73303
Allan Cutler <iamcutler@icloud.com>
2016-10-04 23:10:13 -0700

    fix(talent press): Set link sizing for FF issue

--- app/assets/javascripts/angular/components/ClientPress.js
+++ app/assets/javascripts/angular/components/ClientPress.js
@@ -10,7 +10,7 @@ export default function ClientPress() {
         template: `
             <figure>
                 <div class="thumb">
-                    <a ng-href="{{ destination }}" target="_blank" title="{{ caption }}">
+                    <a ng-href="{{ destination }}" class="talent-press-link" target="_blank" title="{{ caption }}">
                         <img ng-src="{{ img.square }}">
                     </a>
                 </div>
